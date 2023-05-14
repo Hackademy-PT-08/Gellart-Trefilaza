951a10665262c9a935c30fd2e4c7a073ea72c413
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-05-14 19:01:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//exercise 1//
+/*
+let moovieTitle = `Pulp Fiction`;
+let moovieYear = `1994`;
+let moovieDirector = `Quentin Tarantino`;
+
+console.log(`movie:  ${moovieTitle} \n year: ${moovieYear} \n director: ${moovieDirector} `);
+
+console.log(`${moovieTitle} (${moovieYear} - ${moovieDirector})`);
+
+//exercise 2//
+let bookTitle;
+bookTitle = `The intelligent investor`
+
+let bookDescription = `A book by Benjamin Graham, this book describe the best attitude in the financial univers `
+
+console.log( `book title: ${bookTitle} \n description: ${bookDescription}`)
+
+//exercise 3//
+let admin = `Gino`;
+let name = admin;
+
+alert(admin);
+
+//exercise 4//
+let username;
+let password;
+
+prompt(username);
+prompt(password);
+
+alert(`Welcome ${prompt(username)} in your peronal area`);*/
